f1f4e17e0826feb043f2f6ed9f25352392e7d5d9
TimCookXXX <timleadofficial@gmail.com>
2023-12-09 16:40:09 +0300

    Message

--- src/routes/categories/categories.component.jsx
+++ src/routes/categories/categories.component.jsx
@@ -1,11 +1,31 @@
 import Category from "../../components/category/category.component";
 import Button from "../../components/button/button.component";
 import "./categories.styles.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectUserInfo } from "../../store/userInfo/user-info.selector";
+import { selectCategories } from "../../store/categories/categories.selector";
+import { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategories } from '../../store/categories/categories.action';
 
 const Categories = () => {
 const userInfo = useSelector(selectUserInfo)
+const dispatch = useDispatch()
+const categories = useSelector(selectCategories)
+
+useEffect(() => {
+    const getCategories = async () => {
+        try {
+            const categoryMap = await getCategoriesAndDocuments('categories')
+            dispatch(setCategories(categoryMap))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getCategories();
+}, [])
+
 
 const handleSubmit = async (event) => {
     event.preventDefault()
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 import { userInfoReducer } from "./userInfo/user-info.reducer";
 import { userDetailsReducer } from "./user-details/user-details.reducer";
+import { categoriesReducer } from "./categories/categories.reducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
     userInfo: userInfoReducer,
-    userDetails: userDetailsReducer
+    userDetails: userDetailsReducer,
+    categories: categoriesReducer
 })
