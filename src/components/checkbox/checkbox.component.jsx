5ba07f52aed9feef06c6eacd91d5a2960ec93c42
tequilex <dron2010xxx@mail.ru>
2023-12-10 13:59:49 +0300

    1

--- src/components/checkbox/checkbox.component.jsx
+++ src/components/checkbox/checkbox.component.jsx
@@ -1,30 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectCheckedCategories } from '../../store/checked-categories/checked-categories.selector'
-import { setCheckedCategories } from '../../store/checked-categories/checked-categories.action'
+import { selectCheckedCategories } from "../../store/checked-categories/checked-categories.selector";
+import { setCheckedCategories } from "../../store/checked-categories/checked-categories.action";
 
 const Checkbox = ({ item }) => {
-  const dispatch = useDispatch()
-  const checkedCategories = useSelector(selectCheckedCategories)
+  const dispatch = useDispatch();
+  const checkedCategories = useSelector(selectCheckedCategories);
 
   const handleChange = (event) => {
-    const { checked } = event.target;
-    dispatch(setCheckedCategories(item));
-    console.log(checkedCategories);
-    // console.log(item);
+    const { checked, name } = event.target;
+
   };
 
-  
+  console.log(checkedCategories);
 
   return (
-      <label className="category">
-        <input
-          name={item.name}
-          type="checkbox"
-          // checked={true}
-          onChange={handleChange}
-        />
-        {item.name}
-      </label>
+    <label className="category">
+      <input
+        name={item.name}
+        type="checkbox"
+        // checked={true}
+        onChange={handleChange}
+      />
+      {item.name}
+    </label>
   );
 };
 export default Checkbox;
